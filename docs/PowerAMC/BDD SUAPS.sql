de934dbf6050b21065a6e1aebd2442129329ba79
Jullerot <jullerot.leo.bts.sio@gmail.com>
2018-01-08 16:08:58 +0100

    modification de la bdd

--- docs/PowerAMC/BDD SUAPS.sql
+++ docs/PowerAMC/BDD SUAPS.sql
@@ -1,33 +1,24 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     18/12/2017 15:42:11                          */
+/* Created on:     08/01/2018 16:06:21                          */
 /*==============================================================*/
 
 
-drop table if exists DATE;
-
-drop table if exists RESERVER;
+drop table if exists RESERVATION;
 
 drop table if exists USERS;
 
 /*==============================================================*/
-/* Table: DATE                                                  */
+/* Table: RESERVATION                                           */
 /*==============================================================*/
-create table DATE
+create table RESERVATION
 (
-   DATERESERV           varchar(255) not null,
-   WE                   bool,
-   primary key (DATERESERV)
-);
-
-/*==============================================================*/
-/* Table: RESERVER                                              */
-/*==============================================================*/
-create table RESERVER
-(
+   IDRESERV             int not null,
    IDUSER               int not null,
-   DATERESERV           varchar(255) not null,
-   primary key (IDUSER, DATERESERV)
+   USE_IDUSER           int,
+   DATEPREVU            varchar(255),
+   DATERESERV           varchar(255),
+   primary key (IDRESERV)
 );
 
 /*==============================================================*/
@@ -35,7 +26,7 @@ create table RESERVER
 /*==============================================================*/
 create table USERS
 (
-   IDUSER               int not null AUTO_INCREMENT,
+   IDUSER               int not null,
    NOM                  varchar(255),
    PRENOM               varchar(255),
    USERNAME             varchar(255),
@@ -52,9 +43,9 @@ create table USERS
    primary key (IDUSER)
 );
 
-alter table RESERVER add constraint FK_RESERVER foreign key (DATERESERV)
-      references DATE (DATERESERV) on delete restrict on update restrict;
+alter table RESERVATION add constraint FK_INVITER foreign key (USE_IDUSER)
+      references USERS (IDUSER) on delete restrict on update restrict;
 
-alter table RESERVER add constraint FK_RESERVER2 foreign key (IDUSER)
+alter table RESERVATION add constraint FK_RESERVER foreign key (IDUSER)
       references USERS (IDUSER) on delete restrict on update restrict;
 
